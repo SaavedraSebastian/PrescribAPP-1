b4b27b5de35e09b7a0dfec177646839e282e2d2e
SaavedraSebastian <165932627+SaavedraSebastian@users.noreply.github.com>
2024-07-28 18:03:13 -0500

    Create script-registro.js

---
+++ script-registro.js
@@ -0,0 +1,39 @@
+let db;
+const request = indexedDB.open("usuariosDB", 1);
+
+request.onupgradeneeded = function(event) {
+    db = event.target.result;
+    const objectStore = db.createObjectStore("usuarios", { keyPath: "email" });
+    objectStore.createIndex("nombre", "nombre", { unique: false });
+    objectStore.createIndex("password", "password", { unique: false });
+};
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+};
+
+request.onerror = function(event) {
+    console.log("Error al abrir la base de datos:", event.target.error);
+};
+
+function registrarUsuario(event) {
+    event.preventDefault();
+    const nombre = document.getElementById('nombreRegistro').value;
+    const email = document.getElementById('emailRegistro').value;
+    const password = document.getElementById('passwordRegistro').value;
+
+    const transaction = db.transaction(["usuarios"], "readwrite");
+    const objectStore = transaction.objectStore("usuarios");
+    const request = objectStore.add({ nombre, email, password });
+
+    request.onsuccess = function() {
+        alert("Usuario registrado con éxito.");
+        window.location.href = "iniciar.html";  
+    };
+
+    request.onerror = function(event) {
+        alert("Error al registrar el usuario:", event.target.error);
+    };
+}
+
+document.getElementById('registroForm').addEventListener('submit', registrarUsuario);
